6f3d6649b3e2d6d01b28749c01e9c7c12db272d4
alvin6233 <dong.alvinme@gmail.com>
2023-07-19 10:23:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from openxlab.model import download
 from openxlab.model import inference
 
 def hello(i):
-    try:
-        result = inference("meijiawen1/test-image", ["./demo_text_ocr.jpg"])
-    except Exception as e:
-        print(f'error:{e}')
+    # try:
+    #     result = inference("meijiawen1/test-image", ["./demo_text_ocr.jpg"])
+    # except Exception as e:
+    #     print(f'error:{e}')
+    download(model_repo='houshaowei/AnimateDiff', model_name=['mm_sd_v15.ckpt','mm_sd_v14.ckpt'])
     classifier = pipeline("sentiment-analysis")
     a = classifier(i)
     return a
