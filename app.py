3366c204c0b535ec66c6370f856200398b57cf7f
alvin6233 <dong.alvinme@gmail.com>
2023-08-15 17:33:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
 from transformers import pipeline
 import gradio as gr
-from openxlab.model import download
-from openxlab.model import inference
 
 def hello(i):
-    # try:
-    #     result = inference("meijiawen1/test-image", ["./demo_text_ocr.jpg"])
-    # except Exception as e:
-    #     print(f'error:{e}')
-    download(model_repo='houshaowei/AnimateDiff', model_name=['mm_sd_v15.ckpt','mm_sd_v14.ckpt'], overwrite=True)
-    classifier = pipeline("sentiment-analysis")
-    a = classifier(i)
-    return a
+    return i
 
 
 iface = gr.Interface(fn=hello, inputs="text", outputs="text")
