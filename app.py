87be940ce175563a11cf95a5c3e3c2e39568e503
alvin6233 <dong.alvinme@gmail.com>
2023-08-15 15:21:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import gradio as gr
 from openxlab.model import download
 
 def hello(i):
-    download(model_repo='dongxiaozhuang/cli_down', model_name=['model_1'], overwrite=True)
+    XLAB_CACHE='/home/xlab-app-center'
+    download(model_repo='LAMM/lamm_llm_7b_v0', model_name=['tokenizer.model'],output=XLAB_CACHE, overwrite=True)
     return i
 
 
