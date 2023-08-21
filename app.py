1edd952895c44f5f05d2adc2b47183ccaf879fde
alvin6233 <dong.alvinme@gmail.com>
2023-08-21 18:50:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from transformers import pipeline
 import gradio as gr
-# With pipeline, just specify the task and the model id from the Hub.
-from transformers import pipeline
-pipe = pipeline("text-generation", model="distilgpt2")
 
 # If you want more control, you will need to define the tokenizer and model.
 from transformers import AutoTokenizer, AutoModelForCausalLM
