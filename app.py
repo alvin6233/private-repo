c20a547c2d76ebd296aa23ec35efb2a1504507c9
alvin6233 <dong.alvinme@gmail.com>
2023-08-25 10:43:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from transformers import pipeline
 import gradio as gr
+import os
 
 # If you want more control, you will need to define the tokenizer and model.
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -8,7 +9,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 def hello(i):
     tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
     model = AutoModelForCausalLM.from_pretrained("distilgpt2")
-    
+    os.remove('.cache/huggingfalce/models--distilgpt2')
     return i
 
 
