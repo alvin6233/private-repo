2be374dc9e900246e994764fb8f9d0ad29076657
alvin6233 <dong.alvinme@gmail.com>
2024-03-15 15:58:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 # If you want more control, you will need to define the tokenizer and model.
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-base_path = './wfbi_repo_3'
+base_path = './test/wfbi_repo_3'
 os.system(f'git clone https://code-in.openxlab.org.cn/dongxiaozhuang/wfbi_repo_3.git {base_path}')
 print(f'git clone finished')
 os.system(f'cd {base_path} && git lfs pull')
