f378234ef4c79f2946452dd35260a4b674fdd4c2
alvin6233 <dong.alvinme@gmail.com>
2024-03-15 15:02:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import os
 # If you want more control, you will need to define the tokenizer and model.
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+base_path = './wfbi_repo_3'
+os.system(f'git clone https://code-in.openxlab.org.cn/dongxiaozhuang/wfbi_repo_3.git {base_path}')
+print(f'git clone finished')
+os.system(f'cd {base_path} && git lfs pull')
+print(f'git lfs pull finished')
 
 def hello(i):
     #os.system('rm -rf /home/xlab-app-center/.cache/huggingface/hub/models--distilgpt2')
